fe3fc84c846b206b5d19f869684e5d3068f78571
caglakar <65131480+caglakar@users.noreply.github.com>
2022-02-17 21:20:48 +0300

    Add Repos

---
+++ src/main/PhoneDirectory/PhoneDirectory.Services.Contact/Repositories/ContactRepository.cs
@@ -0,0 +1,45 @@
+﻿using PhoneDirectory.Services.Contact.DbContexts;
+using PhoneDirectory.Services.Contact.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PhoneDirectory.Services.Contact.Repositories
+{
+    public class ContactRepository : IContactRepository
+    {
+        private readonly ContactDbDontext _contactDbContext;
+
+        public ContactRepository(ContactDbDontext contactDbContext)
+        {
+            _contactDbContext = contactDbContext ?? throw new ArgumentNullException(nameof(contactDbContext));
+        }
+        public void AddContact(Entities.Contact contact)
+        {
+            if (contact == null)
+            {
+                throw new ArgumentNullException(nameof(contact));
+            }
+            _contactDbContext.Contacts.Add(contact);
+        }
+        public Entities.Contact GetContact(Guid contactId)
+        {
+            if (contactId == Guid.Empty)
+            {
+                throw new ArgumentNullException(nameof(contactId));
+            }
+
+            return _contactDbContext.Contacts.FirstOrDefault(a => a.Id == contactId);
+        }
+
+        public IEnumerable<Entities.Contact> GetContacts()
+        {
+            return _contactDbContext.Contacts.ToList<Entities.Contact>();
+        }
+        public bool Save()
+        {
+            return (_contactDbContext.SaveChanges() >= 0);
+        }
+
+    }
+}
---
+++ src/main/PhoneDirectory/PhoneDirectory.Services.Contact/Repositories/IContactRepository.cs
@@ -0,0 +1,19 @@
+﻿using PhoneDirectory.Services.Contact.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PhoneDirectory.Services.Contact.Repositories
+{
+   public interface IContactRepository
+    {
+        IEnumerable<Entities.Contact> GetContacts();
+        Entities.Contact GetContact(Guid contactId);
+
+        void AddContact(Entities.Contact contact);
+
+        bool Save();
+
+    }
+}
